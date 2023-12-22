164233830673ce8efdecf693037b6769d7aef5df
Rajnish <b421037@iiit-bh.ac.in>
2023-12-22 22:26:13 +0530

    react project completed

--- expressjs/routes/index.js
+++ expressjs/routes/index.js
@@ -1,40 +1,36 @@
-const apiKey = require("../middlewares/apiKey");
-
-const router = require("express").Router();
-
-const apiKeyMiddleware = require("../middlewares/apiKey.js");
-
-//router level middleware
+const router = require('express').Router();
+const apiKeyMiddleware = require('../middlewares/apiKey');
 
 // router.use(apiKeyMiddleware);
 
-router.get("/", (req, res) => {
-  res.render("index", {
-    title: "My home page",
-  });
+router.get('/', (req, res) => {
+    res.render('index', {
+        title: 'My home page'
+    });
 });
 
-router.get("/about", (req, res) => {
-  res.render("about", {
-    title: "my about page",
-  });
+router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'My about page'
+    })
 });
 
-router.get("/download", (req, res) => {
-  res.download(path.resolve(__dirname) + "/about.html");
+router.get('/download', (req, res) => {
+    res.download(path.resolve(__dirname) + '/about.html');
 });
 
-router.get("/api/products", apiKeyMiddleware, (req, res) => {
-  res.json([
-    {
-      id: "123",
-      name: "Chrome",
-    },
-    {
-      id: "124",
-      name: "Firefox",
-    },
-  ]);
-});
+// router.get('/api/products', apiKeyMiddleware, (req, res) => {
+//     res.json([
+//         {
+//             id: '123',
+//             name: 'Chrome'
+//         },
+//         {
+//             id: '124',
+//             name: 'Firefox'
+//         }
+//     ])
+// });
+
 
 module.exports = router;
--- expressjs/server.js
+++ expressjs/server.js
@@ -1,23 +1,37 @@
-const express = require("express");
-
+const express = require('express');
+const path = require('path');
 const app = express();
+const mainRouter = require('./routes/index');
+const productRouter = require('./routes/products');
+const ErrorHandler = require('./errors/ErrorHandler');
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.port || 3000;
 
-const path = require("path");
-
-const mainRouter = require("./routes/index");
-
-//ejs template engine
-app.set("view engine", "ejs");
-
-// console.log(app.get("view engine"));
-// console.log(app.get("views"));
-
-app.use(express.static("public"));
-
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+app.use(productRouter);
 app.use(mainRouter);
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.use((req, res, next) => {
+    return res.json({ message: 'page not found!'});
 });
+
+app.use((err, req, res, next) => {
+    if (err instanceof ErrorHandler) {
+        res.status(err.status).json({
+            error: {
+                message: err.message,
+                status: err.status
+            }
+        });
+    } else {
+        res.status(500).json({
+            error: {
+                message: err.message,
+                status: err.status
+            }
+        });
+    }
+});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
